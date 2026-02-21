b06a5ea0fa05c22204f1bf7406ec5ee049fac4a9
Spandana Kona <spandanakona017@gmail.com>
2026-02-21 11:23:52 +0530

    New commit

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.login_view, name='login'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('teacher-dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
+    path('student-dashboard/', views.student_dashboard, name='student_dashboard'),
+]
--- analytics_app/views.py
+++ analytics_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login_view(request):
+    return render(request, 'accounts/login.html')
+
+def admin_dashboard(request):
+    return render(request, 'accounts/admin_dashboard.html')
+
+def teacher_dashboard(request):
+    return render(request, 'accounts/teacher_dashboard.html')
+
+def student_dashboard(request):
+    return render(request, 'accounts/student_dashboard.html')
